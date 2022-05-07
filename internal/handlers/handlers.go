74869526ef1ffd5b622840202a4605802c8b9235
alirah <ali@govassist.com>
2022-05-07 06:00:00 +0330

    complete project

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,186 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"sort"
+)
+
+var wsChan = make(chan WsPayload)
+var clients = make(map[WebsocketConnection]string)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("../../html"),
+	jet.InDevelopmentMode(),
+)
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+type WebsocketConnection struct {
+	*websocket.Conn
+}
+
+type WsJsonResponse struct {
+	Action         string   `json:"action"`
+	Message        string   `json:"message"`
+	MessageType    string   `json:"message_type"`
+	ConnectedUsers []string `json:"connected_users"`
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"Username"`
+	Message  string              `json:"message"`
+	Conn     WebsocketConnection `json:"-"`
+}
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected to endpoint")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+	response.ConnectedUsers = getUserList()
+	response.Action = "list_users"
+
+	conn := WebsocketConnection{Conn: ws}
+	clients[conn] = ""
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+	}
+
+	go ListenForWs(&conn)
+}
+
+func ListenForWs(conn *WebsocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error: ", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	var response WsJsonResponse
+
+	for {
+		e := <-wsChan
+
+		switch e.Action {
+		case "username":
+			clients[e.Conn] = e.Username
+			users := getUserList()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
+		case "left":
+			// notify other users
+			response.Action = "left"
+			response.Message = fmt.Sprintf("%s left the chat", e.Username)
+			if e.Username != "" {
+				broadcastToOthers(response, e.Conn)
+			}
+
+			// list of the online users
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</string>: %s </br>", e.Username, e.Message)
+			broadcastToAll(response)
+
+		default:
+
+		}
+	}
+}
+
+func getUserList() []string {
+	var userList []string
+	for _, u := range clients {
+		if u != "" {
+			userList = append(userList, u)
+		}
+	}
+	sort.Strings(userList)
+	return userList
+}
+
+func broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket err")
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+func broadcastToOthers(response WsJsonResponse, user WebsocketConnection) {
+	delete(clients, user)
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket err")
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmpl)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
